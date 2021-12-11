ba7a80bae88c7f6f14c83a9868250656b25492b4
Milos Paunovic <paun992@hotmail.com>
2021-12-11 20:33:56 +0100

    Default Button global component

--- src/boot/components.js
+++ src/boot/components.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
 import Input from '../components/forms/Input';
+import Button from '../components/forms/Button';
 
 Vue.component('Input', Input);
+Vue.component('Button', Button);
---
+++ src/components/forms/Button.vue
@@ -0,0 +1,11 @@
+<script>
+import { QBtn } from 'quasar';
+
+export default {
+  extends: QBtn,
+  props: {
+    outline: { type: Boolean, default: true },
+    color: { type: String, default: 'primary' },
+  },
+};
+</script>
