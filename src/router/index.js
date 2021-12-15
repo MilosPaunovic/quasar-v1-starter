939db654a3697c7effcbf8fa5a4672500a23bf79
Milos Paunovic <paun992@hotmail.com>
2021-12-15 12:55:21 +0100

    Directly exporting router & store instances

--- src/router/index.js
+++ src/router/index.js
@@ -5,40 +5,25 @@ import routes from './routes';
 
 Vue.use(VueRouter);
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
+const router = new VueRouter({
+  scrollBehavior: () => ({ x: 0, y: 0 }),
+  routes,
+  mode: process.env.VUE_ROUTER_MODE,
+  base: process.env.VUE_ROUTER_BASE,
+});
 
-export default function () {
-  const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
-    routes,
+router.beforeEach((to, from, next) => {
+  const isPublic = to.matched.some((record) => record.meta.public);
+  const onlyLoggedOut = to.matched.some((record) => record.meta.onlyLoggedOut);
+  const loggedIn = USER_SERVICE.getUser();
 
-    // Leave these as they are and change in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    mode: process.env.VUE_ROUTER_MODE,
-    base: process.env.VUE_ROUTER_BASE,
-  });
+  // If not authenticated, redirect to Login
+  if (!isPublic && !loggedIn) return next({ name: 'Login' });
 
-  Router.beforeEach((to, from, next) => {
-    const isPublic = to.matched.some((record) => record.meta.public);
-    const onlyLoggedOut = to.matched.some((record) => record.meta.onlyLoggedOut);
-    const loggedIn = USER_SERVICE.getUser();
+  // If authenticated, don't allow access to public pages
+  if (loggedIn && onlyLoggedOut) return next({ name: 'Home' });
 
-    // If not authenticated, redirect to Login
-    if (!isPublic && !loggedIn) return next({ name: 'Login' });
+  return next();
+});
 
-    // If authenticated, don't allow access to public pages
-    if (loggedIn && onlyLoggedOut) return next({ name: 'Home' });
-
-    return next();
-  });
-
-  return Router;
-}
+export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -6,26 +6,9 @@ import auth from './auth';
 
 Vue.use(Vuex);
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
+const store = new Vuex.Store({
+  modules: { global, auth },
+  strict: process.env.DEBUGGING,
+});
 
-export default function (/* { ssrContext } */) {
-  const Store = new Vuex.Store({
-    modules: {
-      global,
-      auth,
-    },
-
-    // enable strict mode (adds overhead!)
-    // for dev mode only
-    strict: process.env.DEBUGGING,
-  });
-
-  return Store;
-}
+export default store;
