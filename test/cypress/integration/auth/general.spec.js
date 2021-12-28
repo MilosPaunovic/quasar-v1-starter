83c8e26821723a906be03c75e51c9c5d31e30ef0
Milos Paunovic <paun992@hotmail.com>
2021-12-28 12:55:52 +0100

    Specified test cases for color mode toggle

---
+++ test/cypress/integration/auth/general.spec.js
@@ -0,0 +1,11 @@
+describe('General', () => {
+  beforeEach(() => cy.logout());
+
+  it('Check if there is a color mode toggle in header.', () => { });
+
+  it('Check if color mode could be switched to dark and back to light.', () => { });
+
+  it('Check what happens if no mode parameter exists in local storage.', () => { });
+
+  it('Check if color mode is being set up to dark as default.', () => { });
+});
