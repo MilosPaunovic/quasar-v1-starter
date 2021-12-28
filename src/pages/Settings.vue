4319afc41fb00d79f499b28f10dbb6f4d4204f5f
Milos Paunovic <paun992@hotmail.com>
2021-12-28 13:44:15 +0100

    Settings page list responsiveness

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,7 +1,18 @@
 <template>
   <q-page class="flex flex-center column">
-    <q-list>
-      <q-item>
+    <q-list class="row full-width">
+      <q-item
+        class="
+          col-xs-10
+          offset-xs-1
+          col-sm-8
+          offset-sm-2
+          col-md-6
+          offset-md-3
+          col-lg-4
+          offset-lg-4
+        "
+      >
         <q-item-section side>
           <DarkToggle :show-icons="false" />
         </q-item-section>
