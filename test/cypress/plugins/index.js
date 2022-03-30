3d4d48c186235030d01d551e3cc6e110958ac184
Milos Paunovic <paun992@hotmail.com>
2022-03-30 14:37:13 +0200

    Adding multiple resolutions for test running

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -1,6 +1,9 @@
 const VARIABLES = require('../../../variables/parser')();
+const RESOLUTIONS = require('../support/resolutions');
 
 module.exports = (on, config) => {
   config.env.ENVIRONMENT = VARIABLES.ENVIRONMENT;
+  config.env.RESOLUTIONS = RESOLUTIONS;
+
   return config;
 };
---
+++ test/cypress/support/resolutions.js
@@ -0,0 +1,49 @@
+module.exports = [
+  // Desktop screens
+  {
+    name: 'Full HD',
+    width: 1920,
+    height: 1080,
+    orientation: 'landscape',
+  },
+  {
+    name: 'HD',
+    width: 1280,
+    height: 720,
+    orientation: 'landscape',
+  },
+  {
+    name: 'MacBook Pro 13"',
+    width: 2560,
+    height: 1600,
+    orientation: 'landscape',
+  },
+
+  // Tablet screens
+  {
+    name: 'iPad Pro',
+    width: 2732,
+    height: 2048,
+    orientation: 'landscape',
+  },
+  {
+    name: 'iPad Pro',
+    width: 2048,
+    height: 2732,
+    orientation: 'portrait',
+  },
+
+  // Mobile screens
+  {
+    name: 'iPhone 13 Pro',
+    width: 2532,
+    height: 1170,
+    orientation: 'landscape',
+  },
+  {
+    name: 'iPhone 13 Pro',
+    width: 1170,
+    height: 2532,
+    orientation: 'portrait',
+  },
+];
