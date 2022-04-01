2ce8b35c5a730a7447bf1c2dbf46c5063bda97b5
Milos Paunovic <paun992@hotmail.com>
2022-04-01 12:01:03 +0200

    Tweaked resolutions for testing

--- test/cypress/support/resolutions.js
+++ test/cypress/support/resolutions.js
@@ -1,49 +1,46 @@
+const label = (name, orientation) => `Testing on ${name} screen in ${orientation} mode`;
+
 module.exports = [
-  // Desktop screens
   {
-    name: 'Full HD',
     width: 1920,
     height: 1080,
-    orientation: 'landscape',
+    type: 'desktop',
+    message: label('Full High Definition', 'landscape'),
   },
   {
-    name: 'HD',
     width: 1280,
     height: 720,
-    orientation: 'landscape',
+    type: 'desktop',
+    message: label('High Definition', 'landscape'),
   },
   {
-    name: 'MacBook Pro 13"',
     width: 2560,
     height: 1600,
-    orientation: 'landscape',
-  },
-
-  // Tablet screens
-  {
-    name: 'iPad Pro',
-    width: 2732,
-    height: 2048,
-    orientation: 'landscape',
+    type: 'desktop',
+    message: label('MacBook', 'landscape'),
   },
   {
-    name: 'iPad Pro',
-    width: 2048,
-    height: 2732,
-    orientation: 'portrait',
-  },
-
-  // Mobile screens
-  {
-    name: 'iPhone 13 Pro',
-    width: 2532,
-    height: 1170,
-    orientation: 'landscape',
+    width: 1180,
+    height: 820,
+    type: 'tablet',
+    message: label('iPad', 'landscape'),
   },
   {
-    name: 'iPhone 13 Pro',
-    width: 1170,
-    height: 2532,
-    orientation: 'portrait',
+    width: 820,
+    height: 1180,
+    type: 'tablet',
+    message: label('iPad', 'portrait'),
+  },
+  {
+    width: 844,
+    height: 390,
+    type: 'mobile',
+    message: label('iPhone', 'landscape'),
+  },
+  {
+    width: 390,
+    height: 844,
+    type: 'mobile',
+    message: label('iPhone', 'portrait'),
   },
 ];
